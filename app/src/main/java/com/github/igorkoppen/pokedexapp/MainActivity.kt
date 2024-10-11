df64bf0da4545e934a47b4283d8c3bdbb479bb21
Igor Pasqualino <igorpasqualino@gmail.com>
2024-10-11 11:09:05 -0300

    Novo layout

--- app/src/main/java/com/github/igorkoppen/pokedexapp/MainActivity.kt
+++ app/src/main/java/com/github/igorkoppen/pokedexapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.igorkoppen.pokedexapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -11,31 +12,46 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainAdapter: MainAdapter
+    private var hasSearched: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         mainAdapter = MainAdapter()
         binding.rvSprites.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.HORIZONTAL, false
+            this, LinearLayoutManager.HORIZONTAL, false
         )
         binding.rvSprites.adapter = mainAdapter
+        binding.ivBackground.setImageDrawable(null)
+        binding.ivBackground.setBackgroundColor(Color.BLACK)
+
         viewModel.pokemonResponse.observe(this) { pokemon ->
-            binding.tvPokemonName.text = "Nome: ${pokemon.name}"
-            binding.tvPokemonHeight.text = "Altura: ${pokemon.height}"
-            binding.tvPokemonWeight.text = "Peso: ${pokemon.weight}"
-            Glide.with(this).load(pokemon.officialArtwork).into(binding.ivOfficialArtWork)
-            mainAdapter.submitList(pokemon.imagesURL)
+            if (hasSearched) {
+                binding.tvPokemonName.text = "Nome: ${pokemon.name}"
+                binding.tvPokemonHeight.text = "Altura: ${pokemon.height}m"
+                binding.tvPokemonWeight.text = "Peso: ${pokemon.weight}Kg"
+                Glide.with(this)
+                    .load(pokemon.officialArtwork)
+                    .into(binding.ivOfficialArtWork)
+
+                mainAdapter.submitList(pokemon.imagesURL)
+                binding.ivBackground.setBackgroundResource(R.drawable.background_pokedex)
+            }
         }
 
         binding.btnSearch.setOnClickListener {
             val id = binding.etPokemonId.text.toString().toIntOrNull()
             if (id != null) {
                 viewModel.fetchPokemon(id)
-                binding.tvPokemonId.text = "Numero: ${id}"
+                binding.tvPokemonId.text = "#${id}"
+                hasSearched = true
+            } else {
+                hasSearched = false
+                binding.ivBackground.setImageDrawable(null)
+                binding.ivBackground.setBackgroundColor(Color.BLACK)
             }
         }
     }
 }
-
